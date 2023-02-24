e0c65b6cfe2b2f0a4de7267412c5f79de8645691
MariaMilkova <116704409+MariaMilkova@users.noreply.github.com>
2023-02-24 23:54:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,19 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    int result = 1;
+    if (a != b) {
+        while (a != b) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+        result = a;
+        return result;
+    } else {
+        result = a;
+        return result;
+    }
 }
